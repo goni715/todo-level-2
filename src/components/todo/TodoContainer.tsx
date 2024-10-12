9376a25ee1afea4efb7d3deedf00da6e05dd6198
goni715 <halesgoni715149123@gmail.com>
2024-10-12 13:15:12 +0600

    shadcn modal & dropdown-menu

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,22 +1,29 @@
+import AddTodoModal from "../modal/AddTodoModal";
 import TodoCard from "./TodoCard";
+import TodoFilter from "./TodoFilter";
 
 
 const TodoContainer = () => {
     return (
-        <>
-           <div>
-            <div>
-                <button>Add todo</button>
-                <button>Filter</button>
-            </div>
-            <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-3">
-              {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
+      <>
+        <div>
+          <div className="flex justify-between mb-5">
+            <AddTodoModal/>
+            <button>Filter</button>
+            <TodoFilter/>
+          </div>
+          <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px]">
+            {/* <div className="bg-white p-5 text-2xl font-bold flex justify-center items-center rounded-md">
                 <p>There is no task pending</p>
               </div> */}
-              <TodoCard/>
+            <div className="bg-white p-5 w-full h-full rounded-lg space-y-3">
+              <TodoCard />
+              <TodoCard />
+              <TodoCard />
             </div>
-           </div>
-        </>
+          </div>
+        </div>
+      </>
     );
 };
 
