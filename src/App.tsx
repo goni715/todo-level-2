b0374a9ed6a0cdccd5812424b7a8b9ac379a3e4d
goni715 <halesgoni715149123@gmail.com>
2024-10-12 11:04:27 +0600

    shadcn-setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "./components/ui/button";
 
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl text-red-500">This is Todo App</h1>
+      <div className="p-8">
+        <Button>Click me</Button>
+      </div>
     </>
   );
 };
